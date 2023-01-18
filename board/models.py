0cd61d412c854302537db35995a03e81d06740b9
Pupsik2001 <kenguru13422431@gmail.com>
2023-01-18 19:01:36 +0600

    add view board

--- board/models.py
+++ board/models.py
@@ -8,10 +8,11 @@ class Board(models.Model):
     id = models.AutoField(primary_key=True)
     manage = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     board = models.CharField(max_length=100)
+    done = models.BoaleanField(default=False)
 
 
     class Meta:
         ordering = ['id']
 
     def __str__(self):
-        return self.board
+        return self.board + ' - ' + str(self.done)
