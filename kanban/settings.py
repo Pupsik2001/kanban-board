5fefeb3086dea99dd20c6dc5cfa741ac62fe5b63
Pupsik2001 <kenguru13422431@gmail.com>
2023-01-23 17:36:17 +0600

    new model

--- kanban/settings.py
+++ kanban/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
     # my app 
     'board',
-    'accounts',
+    # 'accounts',
 ]
 
 MIDDLEWARE = [
--- kanban/urls.py
+++ kanban/urls.py
@@ -18,19 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 
 from board import urls as board_urls 
-from accounts import urls as auth_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include(board_urls, namespace='board')),
-    path('auth/', include((auth_urls, 'accounts'), namespace='accounts')),
 ]
 
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.STATIC_URL, 
-document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     from django.conf.urls.static import static
+#     urlpatterns += static(settings.STATIC_URL, 
+# document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL,
+# document_root=settings.MEDIA_ROOT)
